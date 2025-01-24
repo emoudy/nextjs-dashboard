5de7395869a51fa1d33ba030f9ae8aea52de08c7
Elda Moudy <elda@moudy.net>
2025-01-24 13:33:28 -0500

    Adding Google fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+// adding custom Google font
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});

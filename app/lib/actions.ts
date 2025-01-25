4c7ec0108d41c111c2a43749c132455944d2d2d8
Elda Moudy <elda@moudy.net>
2025-01-25 15:20:53 -0500

    udating the UpdateInvoice action and adding notes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -39,9 +39,10 @@ export type State = {
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 export async function createInvoice(prevState: State, formData: FormData) {
-  // Validate form using Zod
-  // safeParse() will return an object containing either a success or error field which will help handle 
-  // validation more gracefully without having put this logic inside the try/catch block.
+  /* Validate form using Zod
+    safeParse() will return an object containing either a success or error field which will help handle 
+    validation more gracefully without having put this logic inside the try/catch block. 
+  */
   const validatedFields  = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -80,15 +81,30 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  /* Validate form using Zod
+    safeParse() will return an object containing either a success or error field which will help handle 
+    validation more gracefully without having put this logic inside the try/catch block.
+  */
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+  
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
+  // Insert data into the database
   try {
     await sql`
         UPDATE invoices
@@ -96,9 +112,11 @@ export async function updateInvoice(id: string, formData: FormData) {
         WHERE id = ${id}
       `;
   } catch (error) {
-    console.error(error);
+    // If a database error occurs, return a more specific error.
+    return { message: 'Database Error: Failed to Update Invoice.' };
   }
  
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
